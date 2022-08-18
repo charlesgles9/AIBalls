73ff950d78b85056c4e9f6e6da5d1996c97135b0
charlie <charlesgles9@gmail.com>
2022-08-18 12:43:45 +0300

    created AIBalls repo

--- app/src/main/java/com/neural/aiballs/MainActivity.kt
+++ app/src/main/java/com/neural/aiballs/MainActivity.kt
@@ -2,10 +2,27 @@ package com.neural.aiballs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.graphics.glcanvas.engine.GLCanvasSurfaceView
 
 class MainActivity : AppCompatActivity() {
+    private var surface:GLCanvasSurfaceView?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        surface=GLCanvasSurfaceView(this,Renderer(this,720f,1280f))
+        setContentView(surface)
+    }
+
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        surface?.onRelease()
     }
 }
---
+++ app/src/main/java/com/neural/aiballs/Renderer.kt
@@ -0,0 +1,23 @@
+package com.neural.aiballs
+
+import android.content.Context
+import com.graphics.glcanvas.engine.GLRendererView
+
+class Renderer(private val context: Context,width:Float,height:Float):GLRendererView(width, height) {
+
+
+
+    override fun prepare() {
+
+    }
+
+
+    override fun draw() {
+
+    }
+
+
+    override fun update(delta: Long) {
+
+    }
+}
